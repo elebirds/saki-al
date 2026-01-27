71602c6fb0e94143b432a0d3df99cc3550004fdd
elebirds <hhmcn@outlook.com>
2026-01-27 23:12:45 +0800

    refactor: remove unnecessary router

--- saki-api/src/saki_api/api/api_v1/api.py
+++ saki-api/src/saki_api/api/api_v1/api.py
@@ -1,10 +1,10 @@
+from fastapi import APIRouter
+
 from saki_api.api.api_v1.endpoints import (
     system, login, users, roles
 )
-from saki_api.core.router import create_api_router
 
-# 使用 AutoWrapAPIRoute 创建 API 路由器
-api_router = create_api_router()
+api_router = APIRouter()
 api_router.include_router(system.router, prefix="/system", tags=["system"])
 api_router.include_router(login.router, prefix="/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
